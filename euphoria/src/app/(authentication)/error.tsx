a8574fce938c43cd83f8cc287b872ffc02fd8daf
HuongLe <huong.le@asnet.com.vn>
2025-07-31 23:22:49 +0700

    #168 Implement error and not found page

---
+++ euphoria/src/app/(authentication)/error.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { useEffect } from 'react';
+
+// Components
+import { Error as ErrorComponent } from '@/ui/shared/Error';
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    console.error('Error in authentication group');
+  }, [error]);
+
+  return <ErrorComponent reset={reset} />;
+}
--- euphoria/src/constants/images.ts
+++ euphoria/src/constants/images.ts
@@ -5,6 +5,7 @@ export const IMAGES = {
     'https://images.unsplash.com/photo-1551028719-00167b16eac5?w=400&h=400&fit=crop',
   PREVIEW: process.env.NEXT_PUBLIC_PREVIEW_IMAGE,
   ONBOARDING: '/images/onboarding.webp',
+  NOT_FOUND: '/images/not-found.png',
   FALLBACK_URL: '/images/default-fallback.webp',
   LOGO: '/images/logo.png',
 };
@@ -13,5 +14,9 @@ export const IMAGE_DETAILS = {
   ONBOARDING: {
     src: IMAGES.ONBOARDING,
     alt: 'Three confident women expressing joyful summer vibes.',
+  },
+  NOT_FOUND: {
+    src: IMAGES.NOT_FOUND,
+    alt: 'A 404 error page',
   },
 };
