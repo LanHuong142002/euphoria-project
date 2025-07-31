7aacf92180d365524ff9ef0b55769cdb5c8bbd12
HuongLe <huong.le@asnet.com.vn>
2025-07-31 18:14:11 +0700

    Implement footer component

--- euphoria/src/constants/components.ts
+++ euphoria/src/constants/components.ts
@@ -1,2 +1,7 @@
 export const SCREEN_HEIGHT_WITH_HEADER_MOBILE = 'min-h-[calc(100vh-94px)]';
 export const SCREEN_HEIGHT_WITH_HEADER_DESKTOP = 'lg:min-h-[calc(100vh-115px)]';
+
+export const SCREEN_HEIGHT_WITH_HEADER_AND_FOOTER_MOBILE =
+  'min-h-[calc(100vh-94px-148px)]';
+export const SCREEN_HEIGHT_WITH_HEADER_AND_FOOTER_DESKTOP =
+  'lg:min-h-[calc(100vh-115px-148px)]';
---
+++ euphoria/src/ui/shared/Footer/index.tsx
@@ -0,0 +1,8 @@
+export const Footer = () => (
+  <div className="py-15 bg-background-secondary">
+    <p className="text-center text-lg font-bold text-text-tertiary">
+      Copyright &copy; {new Date().getFullYear()} Euphoria Folks Pvt Ltd. All
+      rights reserved.
+    </p>
+  </div>
+);
