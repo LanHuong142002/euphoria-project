9078e464ec271e9370cf72e8ef1e44cae7e98346
Huong Nguyen Lan Le <huong.le@asnet.com.vn>
2025-08-04 14:38:07 +0700

    #175 Implement empty cart

--- euphoria/src/app/(main)/cart/page.tsx
+++ euphoria/src/app/(main)/cart/page.tsx
@@ -1,5 +1,28 @@
-const CartPage = () => {
-  return <div>CartPage</div>;
+// Actions
+import { getCart } from '@/actions';
+
+// Components
+import { EmptyCart } from './components/EmptyCart';
+
+const CartPage = async () => {
+  const { data: cart } = await getCart();
+
+  console.log('cart', cart);
+
+  const isCartEmpty = !cart || cart.length === 0;
+
+  if (isCartEmpty) {
+    return <EmptyCart />;
+  }
+
+  return (
+    <div>
+      <div>
+        <h1>Cart with items</h1>
+        {/* TODO: Implement cart with items */}
+      </div>
+    </div>
+  );
 };
 
 export default CartPage;
