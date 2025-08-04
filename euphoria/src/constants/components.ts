d9b264e5d3b61de443980b4ceb44e6a425e2e5dd
Huong Nguyen Lan Le <huong.le@asnet.com.vn>
2025-08-04 14:39:48 +0700

    Update footer responsive and responsive of login page

--- euphoria/src/constants/components.ts
+++ euphoria/src/constants/components.ts
@@ -1,7 +1,8 @@
 export const SCREEN_HEIGHT_WITH_HEADER_MOBILE = 'min-h-[calc(100vh-94px)]';
-export const SCREEN_HEIGHT_WITH_HEADER_DESKTOP = 'lg:min-h-[calc(100vh-115px)]';
+export const SCREEN_HEIGHT_WITH_HEADER_DESKTOP =
+  'md:min-h-[calc(100vh-87px)] lg:min-h-[calc(100vh-114px)]';
 
 export const SCREEN_HEIGHT_WITH_HEADER_AND_FOOTER_MOBILE =
-  'min-h-[calc(100vh-94px-147px)]';
+  'min-h-[calc(100vh-63px-140px)]';
 export const SCREEN_HEIGHT_WITH_HEADER_AND_FOOTER_DESKTOP =
   'lg:min-h-[calc(100vh-115px-147px)]';
