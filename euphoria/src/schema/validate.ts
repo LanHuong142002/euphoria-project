213ac46a3bca67910a8009203a36387a08f2cd50
HuongLe <huong.le@asnet.com.vn>
2025-08-06 09:54:13 +0700

    Refactor schema: rename validateRequiredString to validateRequired

--- euphoria/src/schema/validate.ts
+++ euphoria/src/schema/validate.ts
@@ -3,14 +3,14 @@ import { z } from 'zod';
 // Constants
 import { ERROR_MESSAGES, REGEX_EMAIL, REGEX_PASSWORD } from '@/constants';
 
-export const validateRequiredString = z.string({
+export const validateRequired = z.string({
   message: ERROR_MESSAGES.REQUIRED,
 });
 
-export const validateEmail = validateRequiredString.regex(REGEX_EMAIL, {
+export const validateEmail = validateRequired.regex(REGEX_EMAIL, {
   message: ERROR_MESSAGES.INVALID_EMAIL,
 });
 
-export const validatePassword = validateRequiredString.regex(REGEX_PASSWORD, {
+export const validatePassword = validateRequired.regex(REGEX_PASSWORD, {
   message: ERROR_MESSAGES.INVALID_PASSWORD,
 });
